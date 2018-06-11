fd20e84fef4d37eb7d6893181c74a004edba355b
Chase Derr <chase.derr@treehousei.net>
2018-06-11 12:09:18 -0600

    hitting riot api and storing data on state. Needing to push data dynamically to firebase

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './App.css';
+import rawChampions from './champions.json';
+import firebase from 'firebase';
+import {config} from './config';
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      data:[],
+      champions:[]
+    }
+  }
+  componentWillMount(){
+    //initialize firebase
+    firebase.initializeApp(config);
+
+
+    let API_KEY = "RGAPI-e92e6be9-6af2-4d29-b04b-eb5524bfd8b8";
+    let baseURL = "https://na1.api.riotgames.com";
+    let getchamps = "/lol/static-data/v3/champions";
+    let query = "?locale=en_US&champListData=image&tags=image&dataById=false"
+
+    fetch(`${baseURL}${getchamps}${query}&api_key=${API_KEY}`)
+      .then((response) => {
+        return response.json();
+  })
+  .then((myJson) =>  {
+    this.setState({
+      data: myJson.champions
+    })
+  }).catch(err => {
+    console.log(err)
+  })
+  this.setState({
+    data: rawChampions.data
+  })
+  firebase.database().ref("champions").on('value', (data) => {
+    this.setState({
+      champions: data.toJSON()
+    })
+  })
+
+    firebase.database().ref('champions/001').set(
+      {
+        Aatrox: "id: 266, key: 'Aatrox', name: 'Aatrox', title: 'the Darkin Blade', image: {full: 'Aatrox.png',group:'champion',h:48,sprite:'champion0.png',w:48,x:0,y:0}"
+      }
+  ).then(() => {
+    console.log("inserted")
+  }).catch((error) => {
+    console.log(error)
+  })
+  // Aatrox: "id: 266, key: 'Aatrox', name: 'Aatrox', title: 'the Darkin Blade', image: {full: 'Aatrox.png',group:'champion',h:48,sprite:'champion0.png',w:'48',x:0,y:0}"
+  }
+  componentDidMount(){
+  //   let API_KEY = "RGAPI-e92e6be9-6af2-4d29-b04b-eb5524bfd8b8";
+  //   let baseURL = "https://na1.api.riotgames.com";
+  //   let getchamps = "/lol/static-data/v3/champions";
+  //   let query = "?locale=en_US&champListData=image&tags=image&dataById=false"
+
+  //   fetch(`${baseURL}${getchamps}${query}&api_key=${API_KEY}`)
+  //     .then((response) => {
+  //       return response.json();
+  // })
+  // .then((myJson) =>  {
+  //   this.setState({
+  //     data: myJson.champions
+  //   })
+  // }).catch(err => {
+  //   console.log(err)
+  // })
+  // this.setState({
+  //   data: rawChampions.data
+  // })
+}
+
+  render() {
+    console.log(this.state.data);
+    console.log(this.state.champions);
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
