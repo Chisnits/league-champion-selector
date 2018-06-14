14bf6b4935376860925939b6d96a5ae20f48c7ae
Chase Derr <chase.derr@treehousei.net>
2018-06-14 16:37:35 -0600

    images pulling from url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import rawChampions from './champions.json';
 import firebase from 'firebase';
 import {config} from './config';
-import {API_KEY, baseURL, getchamps, query} from './config';
+// import {API_KEY, baseURL, getchamps, query} from './config';
 
 class App extends Component {
   constructor(){
@@ -11,49 +11,95 @@ class App extends Component {
 
     this.state = {
       data:[],
-      champions:[]
+      champions:[],
+      images: []
     }
   }
   componentWillMount(){
     //initialize firebase
     firebase.initializeApp(config);
 
-    fetch(`${baseURL}${getchamps}${query}&api_key=${API_KEY}`)
-      .then((response) => {
-        return response.json();
-  })
-  .then((myJson) =>  {
+  //   fetch(`${baseURL}${getchamps}${query}&api_key=${API_KEY}`)
+  //     .then((response) => {
+  //       return response.json();
+  // })
+  // .then((myJson) =>  {
+  //   this.setState({
+  //     data: myJson.champions
+  //   })
+  // }).catch(err => {
+  //   console.log(err)
+  // })
+  // this.setState({
+  //   data: rawChampions.data
+  // })
+  firebase.database().ref("champions").on("value", (snapshot) => {
     this.setState({
-      data: myJson.champions
+      champions: snapshot.toJSON()
     })
-  }).catch(err => {
-    console.log(err)
   })
+    firebase.database().ref('champions').set(
+      {
+        data: rawChampions
+      }
+  ).then(() => {
+    // console.log("inserted")
+  }).catch((error) => {
+    // console.log(error)
+  })
+  }
+  componentDidMount(){
+    let myImage = document.querySelector('img');
+    let imgstr = this.state.champions.data.data;
+    for(let key in imgstr){
+      fetch(`https://ddragon.leagueoflegends.com/cdn/8.12.1/img/champion/${imgstr[key].image.full}`)
+      .then((response) => {
+        // console.log(response.blob())
+        return response.blob()
+      })
+      .then((myBlob) =>  {
+        var objectURL = URL.createObjectURL(myBlob);
+        myImage.src = objectURL;
+        // console.log(imageArr)
+        this.setState({
+          images: [...this.state.images, objectURL]
+        })
+          // console.log(myImage.src)
+        //   firebase.database().ref("images").on("value", (snapshot) => {
+        //     this.setState({
+        //       images: snapshot
+        //     })
+        //   })
+
+        //   firebase.database().ref('images').set(
+        //     {
+        //       images: myBlob
+        //     }
+        // ).then(() => {
+        //   console.log("inserted")
+        // }).catch((error) => {
+        //   console.log(error)
+        // })
+          
+        }).catch(err => {
+          console.log(err)
+        })
+    }
+ 
   this.setState({
     data: rawChampions.data
   })
-  firebase.database().ref("champions").on('value', (data) => {
-    this.setState({
-      champions: data.toJSON()
-    })
-  })
-
-    firebase.database().ref('champions/001').set(
-      {
-        Aatrox: "id: 266, key: 'Aatrox', name: 'Aatrox', title: 'the Darkin Blade', image: {full: 'Aatrox.png',group:'champion',h:48,sprite:'champion0.png',w:48,x:0,y:0}"
-      }
-  ).then(() => {
-    console.log("inserted")
-  }).catch((error) => {
-    console.log(error)
-  })
-  // Aatrox: "id: 266, key: 'Aatrox', name: 'Aatrox', title: 'the Darkin Blade', image: {full: 'Aatrox.png',group:'champion',h:48,sprite:'champion0.png',w:'48',x:0,y:0}"
+    
   }
   render() {
-    console.log(this.state.data);
-    console.log(this.state.champions);
+    console.log(this.state.images)
+    let arr = this.state.images.map( (item,i)=> (
+          <div><img src={item} /></div>
+  ))
     return (
       <div>
+        {arr}
+        <img className="img" />
       </div>
     );
   }
