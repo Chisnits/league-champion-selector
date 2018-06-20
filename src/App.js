28eae4230b757e3c1ada0239acdf97341ed1532d
Chase Derr <chase.derr@treehousei.net>
2018-06-20 16:22:12 -0600

    images moving to selector on click

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ import './App.css';
 class App extends Component {
   constructor() {
     super()
+
+    this.state = {
+      allySelection: null
+    }
     
     this.getImages = this.getImages.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   getImages(){
     let names = []
@@ -18,17 +23,41 @@ class App extends Component {
       names.push(rawChampions.data[key].image.full)
     })
     for (let i = 0; i < names.sort().length; i++) {
-      images.push(<img src={require("./assets/squares/"+names[i])} alt="" className="img-responsive" key={i} />  );
+      images.push(<img src={require("./assets/squares/"+names[i])} alt="" className="img-responsive square" key={i} onClick={this.handleClick} />  );
     }
         return images
   }
+  handleClick(e){
+    this.setState({
+      allySelection: e.target.getAttribute('src')
+    })
+  }
 
   render() {
+    console.log(this.state.allySelection)
     return ( 
     <div className="wrapper">
+      <section className="allies">
+        <div className="player-selector">
+          <div className="champion-selected"><img src={this.state.allySelection} className="image-reponsive"/></div>
+        </div>
+        <div className="player-selector">
+          <div className="champion-selected"></div>
+        </div>
+        <div className="player-selector">
+          <div className="champion-selected"></div>
+        </div>
+        <div className="player-selector">
+          <div className="champion-selected"></div>
+        </div>
+        <div className="player-selector">
+          <div className="champion-selected"></div>
+        </div>
+      </section>
       <div className="square-container"> 
         {this.getImages()}
       </div>
+      <section className="enemies"></section>
     </div>);
   }
 }
