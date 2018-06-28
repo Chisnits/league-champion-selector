06a7859bc1d1b6563ce4c4ebfd73b3d3a8fd5077
Chase Derr <chase.derr@treehousei.net>
2018-06-28 12:59:39 -0600

    player selection incrementing on a timer

--- src/components/CharacterHover.js
+++ src/components/CharacterHover.js
@@ -2,16 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CharacterHover = props => {
-    console.log(props.activePlayer)
-    if(props.activePlayer === "Player1"){
     return (
             <img src={props.hover ? (props.hover) : require("../assets/knight-placeholder.png")} className="img-responsive" alt=""/>
     );
-} else if (props.activePlayer === "Player2"){
-    return (
-        <img src={props.hover ? (props.hover) : require("../assets/knight-placeholder.png")} className="img-responsive" alt=""/>
-);
-} else return null
 };
 
 CharacterHover.propTypes = {
--- src/components/PlayerSelection.js
+++ src/components/PlayerSelection.js
@@ -3,9 +3,21 @@ import PropTypes from 'prop-types';
 import '../App.css';
 
 const PlayerSelections = props => {
-    return (
-            <img src={props.playerSelection} className="img-responsive" alt="Character Selected"/>
-    );
+    if(props.activePlayer === "player1"){
+
+    }
+    return (<div>
+        {   props.activePlayer === "player1" ?  (
+        <img src={props.playerSelection} className="img-responsive" alt="Character Selected"/>
+            ):(
+        <div></div>
+        )}
+        {   props.activePlayer === "player2" ?  (
+        <img src={props.playerSelection} className="img-responsive" alt="Character Selected"/>
+            ):(
+        <div></div>
+        )}
+    </div>);
 };
 
 PlayerSelections.propTypes = {
